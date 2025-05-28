00036df4619962454089f7702783953235ee58d6
Xu Haotian <514045393@qq.com>
2025-05-28 20:54:10 +0800

    项目初始化

---
+++ src/main/java/com/xhtsoft/tumiaopicturebackend/TumiaoPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xhtsoft.tumiaopicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumiaoPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TumiaoPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhtsoft/tumiaopicturebackend/TumiaoPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhtsoft.tumiaopicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TumiaoPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
