081fecb8bb08fbfdd4002f5c00f13e418a882d36
Xu Haotian <514045393@qq.com>
2025-05-28 22:01:07 +0800

    项目初始化

--- src/main/java/com/xhtsoft/tumiaopicturebackend/TumiaoPictureBackendApplication.java
+++ src/main/java/com/xhtsoft/tumiaopicturebackend/TumiaoPictureBackendApplication.java
@@ -1,9 +1,11 @@
 package com.xhtsoft.tumiaopicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xhtsoft.tumiaopicturebackend.mapper")
 public class TumiaoPictureBackendApplication {
 
     public static void main(String[] args) {
